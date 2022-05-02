86b261ee0836c0dc845678e72cb3d37825f962e0
GacekKosmatek <60139174+GacekKosmatek@users.noreply.github.com>
2022-05-02 22:29:36 +0200

    add a request parameter to index

--- pages/index.py
+++ pages/index.py
@@ -1,10 +1,10 @@
 from fastapi.responses import HTMLResponse
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
 with open("public/index.html", "r") as f:
     index_page = f.read()
 
-async def handler():
+async def handler(request: Request):
     return HTMLResponse(content=index_page)
 
 def setup(router: APIRouter):
